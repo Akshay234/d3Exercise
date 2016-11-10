a64a5b032ef13c86addbc8c537352a1423eb64be
Akshay <kumarsharmaa69@gmail.com>
2016-11-10 18:34:11 +0530

    Implemented number expansion visualisition

---
+++ public/javascript/numberExpansion.js
@@ -0,0 +1,29 @@
+var generateChart = function () {
+  var data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  var font = fontStyleScale();
+  var width = widthSizeScale();
+
+  var numbers = d3.select('.container').selectAll('.box').data(data);
+  numbers.enter().append('div')
+    .classed("box", true)
+    .text(function (d) {
+      return d;
+    })
+    .style("font", function (d) {
+      return font(d);
+    })
+    .style("width", function (d) {
+      return width(d);
+    })
+};
+
+var fontStyleScale = function () {
+  return d3.scaleLinear().domain([0, 10]).range(["bold italic 12px/30px serif", "bold italic 120px/180px serif"]);
+};
+
+var widthSizeScale = function () {
+  return d3.scaleLinear().domain([0, 10]).range([30, 130]);
+};
+
+window.onload = generateChart();

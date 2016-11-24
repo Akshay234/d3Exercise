80caf51b6539fa40d03581a8af8edefb4edf55dd
Akshay <kumarsharmaa69@gmail.com>
2016-11-24 22:09:20 +0530

    removed unnecessary pie charts generator files

--- public/javascript/pieCharts/donutChart.js
+++
@@ -1,7 +0,0 @@
-var generate = function () {
-  var data = [1, 1, 2, 2, 1, 2, 1];
-  var pieChart = new PieChart(data);
-  pieChart.create({innerRadius: 10, colorCategory: d3.schemeCategory20});
-};
-
-window.onload = generate;
--- public/javascript/pieCharts/pieChart.js
+++
@@ -1,8 +0,0 @@
-var generate = function () {
-  var data = [1, 1, 2, 2, 1, 2, 1];
-  var pieChart = new PieChart(data);
-  pieChart.setColorScheme(d3.schemeCategory20);
-  pieChart.create({});
-};
-
-window.onload = generate;

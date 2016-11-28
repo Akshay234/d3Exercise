39a4651bb46d6f38649cb868afe71a6f4d24ef6d
Akshay <kumarsharmaa69@gmail.com>
2016-11-28 14:43:19 +0530

    renamed sinChartUtil to chartUtil

--- public/javascript/sinCharts/chartUtil.js
+++ public/javascript/sinCharts/chartUtil.js
@@ -15,11 +15,11 @@ Chart.prototype.translate = function (x, y) {
   return 'translate(' + x + ',' + y + ')';
 };
 
-Chart.prototype.calculateValue = function(value, domain) {
+Chart.prototype.calculateValue = function (value, domain) {
   return value / 10 * (domain[0] + domain[1]);
 };
 
-Chart.prototype.createDots = function (group,data) {
+Chart.prototype.createDots = function (group, data) {
   var self = this;
   var domain = self.domain;
   group.selectAll('dot')
@@ -39,39 +39,49 @@ Chart.prototype.createArea = function (args) {
   var self = this;
   var domain = this.domain;
   var areaGroup = args.chart.append('g')
-    .attr('transform',  self.translate(MARGIN, MARGIN));
+    .attr('transform', self.translate(MARGIN, MARGIN));
 
   var area = d3.area()
     .curve(args.interpolation || d3.curveLinear)
-    .x(function(d) { return self.xScale(self.calculateValue(d.x, domain.x)); })
-    .y1(function(d) { return self.yScale(self.calculateValue(d.y, domain.y)); })
+    .x(function (d) {
+      return self.xScale(self.calculateValue(d.x, domain.x));
+    })
+    .y1(function (d) {
+      return self.yScale(self.calculateValue(d.y, domain.y));
+    })
     .y0(INNER_HEIGHT);
 
-    areaGroup.append('path').attr('id', 'area').attr('d', area(args.data));
+  areaGroup.append('path').attr('id', 'area').attr('d', area(args.data));
 };
 
 Chart.prototype.createLine = function (args) {
   var self = this;
   var domain = self.domain;
   var lineGroup = args.chart.append('g')
-    .attr('transform',  self.translate(MARGIN, MARGIN));
+    .attr('transform', self.translate(MARGIN, MARGIN));
 
-  var line =  d3.line()
+  var line = d3.line()
     .curve(args.interpolation || d3.curveLinear)
-    .x(function(d){return (self.xScale(self.calculateValue(d.x, domain.x)))})
-    .y(function(d){return self.yScale(self.calculateValue(d.y, domain.y))});
+    .x(function (d) {
+      return (self.xScale(self.calculateValue(d.x, domain.x)))
+    })
+    .y(function (d) {
+      return self.yScale(self.calculateValue(d.y, domain.y))
+    });
 
   lineGroup.append('path').attr('id', args.id).attr('d', line(args.data));
   return lineGroup;
 };
 
-Chart.prototype.createAxis = function (domain) {
+Chart.prototype.createAxis = function (domain, overrideScale) {
   this.domain = domain;
   var chart = d3.select('.container').append('svg')
     .attr('width', WIDTH)
     .attr('height', HEIGHT);
 
-  var x = this.xScale = d3.scaleLinear()
+  var scale = overrideScale || d3.scaleLinear();
+
+  var x = this.xScale = scale
     .domain(domain.x)
     .range([0, INNER_WIDTH]);
 
@@ -94,4 +104,34 @@ Chart.prototype.createAxis = function (domain) {
     .classed('xAxis', true);
 
   return chart;
+};
+
+Chart.prototype.createHistogram = function (data, type) {
+  var xScale = this.xScale;
+  var yScale = this.yScale;
+  var translate = this.translate;
+
+  var histogram = d3.histogram()
+    .value(function (d) {
+      return type;
+    })
+    .domain(xScale.domain());
+
+  var bars = histogram(data);
+
+  var svg = d3.select('.container').select('svg');
+
+  svg.selectAll("rect")
+    .data(bars)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", 1)
+    .attr('transform', function(d){
+      translate(MARGIN, HEIGHT- MARGIN);
+    })
+    // .attr("transform", function(d) {
+    //   return "translate(" + xScale(d) + "," + yScale(d) + ")"; })
+    .attr("width", function(d) { return xScale(d)})
+    .attr("height", function(d) { return yScale(d) });
+
 };
